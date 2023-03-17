0233ec571722c3637fd2ff9806eaf8ba9639fc94
ntwigamartin <ntwiga.martin@gmail.com>
2023-03-17 17:41:24 +0300

    Completed, all tests passed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+
+    def show
+        user = User.find(params[:id])
+        render json: user, include: :items
+    end
+
+    private
+
+    def not_found_response
+        render json: { error: 'User not found' }, status: :not_found
+    end
 end
